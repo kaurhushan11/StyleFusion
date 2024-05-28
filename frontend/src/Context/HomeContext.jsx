e28c2372380a9dfd46f9c1b9fd521280365d1ef5
kaurhushan11 <102655942+kaurhushan11@users.noreply.github.com>
2024-05-28 22:17:34 +0530

    Update HomeContext.jsx

--- frontend/src/Context/HomeContext.jsx
+++ frontend/src/Context/HomeContext.jsx
@@ -17,10 +17,10 @@ const HomeContextProvider = (props)=>{
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
     useEffect(()=>{
-        fetch('http://localhost:4000/allproducts').then((response)=>response.json()).then((data)=>setAll_product(data))
+        fetch('https://style-fusion-api.vercel.app/allproducts').then((response)=>response.json()).then((data)=>setAll_product(data))
 
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/getcart',{
+            fetch('https://style-fusion-api.vercel.app/getcart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -35,7 +35,7 @@ const HomeContextProvider = (props)=>{
     const addToCart = (itemId)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/addtocart',{
+            fetch('https://style-fusion-api.vercel.app/addtocart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -50,7 +50,7 @@ const HomeContextProvider = (props)=>{
     const removeFromCart = (itemId)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/removefromcart',{
+            fetch('https://style-fusion-api.vercel.app/removefromcart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
